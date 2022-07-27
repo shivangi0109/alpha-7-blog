2cc79c388f3cfbcab611e61822ff53b97127ee7b
Shivangi Srivastava <shi06dec@gmail.com>
2022-07-27 01:42:23 -0600

    Create new article, display validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 end
