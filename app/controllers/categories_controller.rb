9dd1a945a53c258cd574c96f88e635d7c5f69c20
Shivangi srivastava <shi06dec@gmail.com>
2022-09-29 02:02:27 -0600

    Functional test - Create category and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,29 @@
 class CategoriesController < ApplicationController
   def index
-
+    
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else 
+      render "new"
+    end
   end
 
   def show
-    
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
